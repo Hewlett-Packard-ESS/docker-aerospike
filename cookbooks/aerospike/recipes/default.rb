afba0dd3a6b73ae6ad1d40b95e6c94e83fe53e49
Karl Stoney <karl.stoney@hp.com>
2015-04-29 10:38:23 +0100

    Initial config template, some environment variables, docker compose file

--- cookbooks/aerospike/recipes/default.rb
+++ cookbooks/aerospike/recipes/default.rb
@@ -1,3 +1,23 @@
+def default_value(val, default)
+  if val.nil?
+    return default
+  end
+  if val.is_a? Numeric
+    return BigDecimal.new(val).to_i
+  elsif val.downcase === 'true'
+    return true
+  elsif val.downcase === 'false'
+    return false
+  else
+    return val
+  end
+end
+
+cfg = {
+	"namespace" => default_value(ENV['as_namespace'], 'docker'),
+	"ttl" => default_value(ENV['as_ttl'], '30d')	
+}
+
 template '/usr/local/bin/run-aerospike.sh' do
 	source  'run-aerospike.sh.erb'
 	mode '0755'
@@ -9,6 +29,7 @@ end
 template '/storage/aerospike.conf' do
 	source  'aerospike.conf.erb'
 	mode '0755'
+	variables cfg 
 	owner   'docker'
 	group   'docker'
 	action   :create_if_missing
